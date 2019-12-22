6a366a67783f983672204e5d5260041ac20e0724
Kateryna Strough <katerynastrough@hotmail.com>
2019-12-22 18:54:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const fi = (function() {
 
     each: function(collection, iteratee) {
           const newCollection = (collection instanceof Array) ? collection.slice() : Object.values(collection)
-    
+
           for (let idx = 0; idx < newCollection.length; idx++)
             iteratee(newCollection[idx])
-    
+
           return collection
       },
 
