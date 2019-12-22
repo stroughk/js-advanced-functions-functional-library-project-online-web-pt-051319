3f25c0245c0151b65200fb59ce3715f85f158539
Kateryna Strough <katerynastrough@hotmail.com>
2019-12-22 18:55:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,71 +28,71 @@ const fi = (function() {
 
         reduce: function(c = [], callback = () => {}, acc) {
         			let collection = c.slice(0)
-        
+
         			if (!acc) {
         				acc = collection[0]
         				collection = collection.slice(1)
         			}
-        
+
         			let len = collection.length;
-        
+
         			for (let i = 0; i < len; i++) {
         				acc = callback(acc, collection[i], collection)
         			}
         			return acc;
         		},
-        
+
             find: function(collection, predicate) {
               if (!(collection instanceof Array))
                 collection = Object.values(collection)
-        
+
               for (let idx = 0; idx < collection.length; idx++)
                 if (predicate(collection[idx])) return collection[idx]
-        
+
               return undefined
             },
-        
+
             filter: function(collection, predicate) {
               if (!(collection instanceof Array))
                 collection = Object.values(collection)
-        
+
               const newArr = []
-        
+
               for (let idx = 0; idx < collection.length; idx++)
                 if (predicate(collection[idx])) newArr.push(collection[idx])
-        
+
               return newArr
             },
-        
+
             size: function(collection) {
               return (collection instanceof Array) ? collection.length : Object.keys(collection).length
             },
-        
+
             first: function(collection, stop=false) {
               return (stop) ? collection.slice(0, stop) : collection[0]
             },
-        
+
             last: function(collection, start=false) {
               return (start) ? collection.slice(collection.length-start, collection.length) : collection[collection.length-1]
             },
-        
+
             compact: function(collection) {
               const badBad = new Set([false, null, 0, "", undefined, NaN])
               return collection.filter(el => !badBad.has(el))
             },
-        
+
             sortBy: function(collection, callback) {
               const newArr = [...collection]
               return newArr.sort(function(a, b) {
                 return callback(a) - callback(b)
               })
             },
-        
+
             unpack: function(receiver, arr) {
               for (let val of arr)
                 receiver.push(val)
             },
-        
+
             flatten: function(collection, shallow, newArr=[]) {
               if (!Array.isArray(collection)) return newArr.push(collection)
               if (shallow) {
@@ -105,7 +105,7 @@ const fi = (function() {
               }
               return newArr
             },
-        
+
             uniqSorted: function(collection, iteratee) {
               const sorted = [collection[0]]
               for (let idx = 1; idx < collection.length; idx++) {
@@ -114,7 +114,7 @@ const fi = (function() {
               }
               return sorted
             },
-        
+
             uniq: function(collection, sorted=false, iteratee=false) {
               if (sorted) {
                 return fi.uniqSorted(collection, iteratee)
@@ -133,7 +133,7 @@ const fi = (function() {
                 return Array.from(uniqVals)
               }
             },
-        
+
             keys: function(obj) {
               // Using for loop
               const keys = []
@@ -142,7 +142,7 @@ const fi = (function() {
               }
               return keys
             },
-        
+
             values: function(obj) {
               // Using for loop
               const values = []
@@ -150,25 +150,25 @@ const fi = (function() {
                 values.push(obj[key])
               }
               return values
-        
+
               // Using the custom 'map' method from above
               // return this.map(obj, (value) => value)
-        
+
             },
-        
+
             functions: function(obj) {
               const functionNames = []
-        
+
               for (let key in obj) {
                 if (typeof obj[key] === "function"){
                   functionNames.push(key)
                 }
               }
-        
+
               return functionNames.sort()
             },
-        
+
           }
         })()
-        
+
         fi.libraryMethod()
