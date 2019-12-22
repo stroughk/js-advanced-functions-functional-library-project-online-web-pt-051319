9a9f3274b146cc5119c4c6ff857260611c843b40
Kateryna Strough <katerynastrough@hotmail.com>
2019-12-22 18:54:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
-    },
+    each: function(collection, iteratee) {
+          const newCollection = (collection instanceof Array) ? collection.slice() : Object.values(collection)
+    
+          for (let idx = 0; idx < newCollection.length; idx++)
+            iteratee(newCollection[idx])
+    
+          return collection
+      },
 
     map: function() {
 
